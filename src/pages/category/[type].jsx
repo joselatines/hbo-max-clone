4956e876499397d9ae080ff8944b5d218c171a59
DoctorDraxter <doctordraxter@gmail.com>
2022-05-16 11:30:46 -0400

    prev an dnext button in ningle page media added

--- src/pages/category/[type].jsx
+++ src/pages/category/[type].jsx
@@ -1,19 +1,26 @@
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-import MediaCard from '../../components/MediaCard';
 import { getPopular } from '../../services/media';
+import MediaCard from '../../components/MediaCard';
+import Button from '../../components/shared/Button/styles';
 
 export default function Category() {
 	const [mediaData, setMediaData] = useState([{}]); // Where the media data is going to bed saved
+	const [page, setPage] = useState(1);
 
 	const router = useRouter();
-	const { type } = router.query; // Get the media id
+	const { type } = router.query; // Get the media type
 
 	const txt = type === 'movie' ? 'movies' : 'TV Series';
 
+	const changePage = direction => {
+		if (direction === 'next') if (page < 1000) setPage(prev => prev + 1);
+		if (direction === 'prev') if (page > 1) setPage(prev => prev - 1);
+	};
+
 	useEffect(() => {
-		getPopular(type).then(res => setMediaData(res));
-	}, [type]);
+		getPopular(type, page).then(res => setMediaData(res));
+	}, [type, page]);
 
 	return (
 		<div>
@@ -22,30 +29,12 @@ export default function Category() {
 				{mediaData &&
 					mediaData.map(({ poster_path, id }) => (
 						<div style={{ maxWidth: '10rem' }} key={id}>
-							<MediaCard image={poster_path} id={id} redirect={true}>
-								Hola
-							</MediaCard>
+							<MediaCard image={poster_path} id={id} redirect={true} />
 						</div>
 					))}
 			</div>
+			<Button onClick={() => changePage('prev')}>Prev</Button>
+			<Button onClick={() => changePage('next')}>Next</Button>
 		</div>
 	);
 }
-
-/* export async function getStaticPaths() {
-	return {
-		paths: [
-			// String variant:
-			'/media/951470',
-			// Object variant:
-			{ params: { id: '324668' } },
-		],
-		fallback: true,
-	};
-}
-
-export async function getStaticProps(ctx) {
-	return {
-		props: {}, // will be passed to the page component as props
-	};
-} */
--- src/pages/media/[id].jsx
+++ src/pages/media/[id].jsx
@@ -15,21 +15,3 @@ export default function MediaPage() {
 
 	return <SingleMediaPage mediaData={mediaData} />;
 }
-
-export async function getStaticPaths() {
-	return {
-		paths: [
-			// String variant:
-			'/media/951470',
-			// Object variant:
-			{ params: { id: '324668' } },
-		],
-		fallback: true,
-	};
-}
-
-export async function getStaticProps(ctx) {
-	return {
-		props: {}, // will be passed to the page component as props
-	};
-}
